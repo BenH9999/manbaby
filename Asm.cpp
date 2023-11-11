5be4a00665b2f4636b8bf245307789453a0e95d2
BenH9999 <ben.houghton7070@outlook.com>
2023-11-11 04:00:13 +0000

    remove comments

--- Asm.cpp
+++ Asm.cpp
@@ -1,9 +1,55 @@
 #include "Asm.hpp"
 
 int main(){
-    readAsmFromFile();
+    processAll();
+
+    for (const std::string& line : asmFromFile) {
+        std::cout << line << std::endl;
+    }
 
     return 0;
+}
+
+void processAll(){
+    readAsmFromFile();
+    removeCommentsAndEmptyLines();
+    mapAndRemoveLabels();
+}
+
+void removeCommentsAndEmptyLines() {
+    for (std::string& s : asmFromFile) {
+        size_t pos = s.find(';');
+        if (pos != std::string::npos) {
+            s = s.substr(0, pos);
+        }
+    }
+    asmFromFile.erase(
+        std::remove_if(asmFromFile.begin(), asmFromFile.end(),
+            [](const std::string& line) {
+                return line.find_first_not_of(" \r\t\n") == std::string::npos;
+            }
+        ),
+        asmFromFile.end()
+    );
+
+}
+
+void mapAndRemoveLabels(){
+    int address = 0;
+    for(std::string& s : asmFromFile){
+        size_t pos = s.find(':');
+        if(pos != std::string::npos){
+            labelsFromAsm[s.substr(0,pos)] = address;
+            //s.replace(0,pos+1,std::to_string(address)+":");
+        }
+        address++;
+    }
+    for(std::string& s : asmFromFile){
+        for(std::pair<std::string,int> label : labelsFromAsm){
+            size_t pos = s.find(label.first);
+            //if(pos != )
+        }
+    }
 }
 
 void readAsmFromFile(){
--- Asm.hpp
+++ Asm.hpp
@@ -3,7 +3,13 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <algorithm>
+#include <unordered_map>
 
 std::vector<std::string> asmFromFile;
+std::unordered_map<std::string, int> labelsFromAsm;
 
+void processAll();
 void readAsmFromFile();
+void removeCommentsAndEmptyLines();
+void mapAndRemoveLabels();
